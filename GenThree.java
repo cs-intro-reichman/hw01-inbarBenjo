c747fce878dd032f63aee2a75cf074cdfa2f1193
inbarBenjo <154405262+inbarBenjo@users.noreply.github.com>
2023-12-21 08:43:57 +0200

    'test'

--- GenThree.java
+++ GenThree.java
@@ -5,5 +5,19 @@
 public class GenThree {
 	public static void main(String[] args) {
 		// Put your code here	
+		int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+
+        int min = Math.min(a,b); 
+        int max = Math.max(a,b);
+
+        int randnum1 = (int) ((Math.random() * (max - min)) + min);
+        int randnum2 = (int) ((Math.random() * (max - min)) + min);
+        int randnum3 = (int) ((Math.random() * (max - min)) + min);
+        System.out.println(randnum1);
+        System.out.println(randnum2);
+        System.out.println(randnum3);
+        int minrand  = Math.min(randnum1,randnum2);
+        System.out.println("the minimal generated number was" +  Math.min(minrand,randnum3));
 	}
 }
