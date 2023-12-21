74e0ee5b9a71ed495d1eb557fbf7a92de5ea0ea1
inbarBenjo <154405262+inbarBenjo@users.noreply.github.com>
2023-12-21 13:30:08 +0200

    'test'

--- Triangle.java
+++ Triangle.java
@@ -6,5 +6,23 @@
 public class Triangle {
 	public static void main(String[] args) {
 		// Put your code here	
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int c = Integer.parseInt(args[2]);
+
+		//int maxab = Math.max(a,b); 
+		//int max = Math.max(maxab,c);
+		if ( (a+b)> c && (a+c)> b && (b+c)> a)
+		{
+			System.out.println(a + ", " + b + ", " + c + ": " + "true");
+		}
+		else 
+		{
+		   System.out.println(a + ", " + b + ", " + c + ": " + "false");
+		}
+
+
+
+		
 	}
 }
